5c3ddbdca877ddb498a8c992b4a5b41dfc3d2c6c
SAMANTA NAYAK <samanta.nayak@gmail.com>
2025-11-13 07:20:26 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 set -e
-echo "🚀 Starting MAHSR-T3-DPR Streamlit service..."
-rm -rf ~/.streamlit/static
-streamlit cache clear || true
-streamlit run app.py --server.port=$PORT --server.address=0.0.0.0 --server.enableXsrfProtection=false
-
-#!/bin/bash
-set -e
 
 echo "🚀 Starting MAHSR-T3-DPR Streamlit Service on Render..."
 
