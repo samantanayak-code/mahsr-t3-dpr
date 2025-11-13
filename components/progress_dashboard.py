d60a4f9e16988653670a028cb4576122337cb286
SAMANTA NAYAK <samanta.nayak@gmail.com>
2025-11-13 19:22:47 +0530

    Update progress_dashboard.py

--- components/progress_dashboard.py
+++ components/progress_dashboard.py
@@ -1,8 +1,3 @@
-from components.progress_dashboard import show_progress_dashboard
-# ... in the role routing/menu:
-if st.session_state.user_role == "project_manager":
-    show_progress_dashboard()
-# or call from navigation menu wherever appropriate
 # components/progress_dashboard.py
 import io
 from datetime import date, datetime, timedelta
